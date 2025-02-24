754db3764c57502a39a495ea8d22f8e1059dd48b
nekoo7 <abylaiooaooa@mail.ru>
2025-02-24 10:30:29 +0500

    scripts

--- Assets/Scripts/Character/FirstPersonController.cs
+++ Assets/Scripts/Character/FirstPersonController.cs
@@ -7,7 +7,7 @@ using Random = UnityEngine.Random;
 public class FirstPersonController : MonoBehaviour, IDieable
 {
     public bool canMove = true;
-    private bool IsSprinting => canSprint && Input.GetKey(sprintKey);
+    public bool IsSprinting => canSprint && Input.GetKey(sprintKey);
     private bool ShouldJump => Input.GetKeyDown(jumpKey) && characterController.isGrounded;
 
     private bool ShouldCrouch =>
@@ -56,7 +56,7 @@ public class FirstPersonController : MonoBehaviour, IDieable
     [SerializeField] private float timeToCrouch = 0.25f;
     [SerializeField] private Vector3 crouchingCenter = new Vector3(0, 0.5f, 0);
     [SerializeField] private Vector3 standingCenter = new Vector3(0, 0, 0);
-    private bool isCrouching;
+    public bool isCrouching;
     private bool duringCrouchAnimation;
     
     [Header("HeadBob Param")] 
@@ -78,12 +78,8 @@ public class FirstPersonController : MonoBehaviour, IDieable
     [SerializeField] private float baseStepSpeed = 0.5f;
     [SerializeField] private float crouchStepMultipler = 1.5f;
     [SerializeField] private float sprintStepMultipler = 0.6f;
-    [SerializeField] private AudioSource footStepAudioSource = default;
-    [SerializeField] private AudioClip[] woodClips = default;
-    [SerializeField] private AudioClip[] metalClips = default;
-    [SerializeField] private AudioClip[] grassClips = default;
-    [SerializeField] private AudioClip[] dirtClips = default;
-
+    [SerializeField] private AudioSource runAudioSource = default;
+   
     private float footstepTimer = 0;
 
     private float GetCurrentOffset => isCrouching ? baseStepSpeed * crouchStepMultipler :
@@ -146,12 +142,12 @@ public class FirstPersonController : MonoBehaviour, IDieable
             if(canUseHeadBob)
                 HandleHeadBob();
 
-            if (useFootsteps)
-                HandleFootsteps();
+            // if (useFootsteps)
+            //     HandleFootsteps();
             
             UpdateMovementState();
             ApplyFinalMovements();
-            
+            HandleRunningSound();
            // if (characterController.velocity.y <  -1 && characterController.isGrounded)
              //   moveDirection.y = 0;
         }
@@ -191,7 +187,23 @@ public class FirstPersonController : MonoBehaviour, IDieable
 
         
     }
-
+    private void HandleRunningSound()
+    {
+        if (IsSprinting && characterController.isGrounded)
+        {
+            if (!runAudioSource.isPlaying)
+            {
+                runAudioSource.Play();
+            }
+        }
+        else
+        {
+            if (runAudioSource.isPlaying)
+            {
+                runAudioSource.Stop();
+            }
+        }
+    }
     private void HandleJump()
     {
         {
@@ -270,38 +282,39 @@ public class FirstPersonController : MonoBehaviour, IDieable
         }
     }
 
-    private void HandleFootsteps()
-    {
-        if(!characterController.isGrounded) return;
-        if (currentInput == Vector2.zero) return;
+    // private void HandleFootsteps()
+    //    {
+    //        if(!characterController.isGrounded) return;
+    //        if (currentInput == Vector2.zero) return;
+    //
+    //        footstepTimer -= Time.deltaTime;
+    //
+    //        if (footstepTimer <= 0)
+    //        {
+    //            if (Physics.Raycast(playerCamera.transform.position, Vector3.down, out RaycastHit hit, 3))
+    //            {
+    //                switch (hit.collider.tag)
+    //                {
+    //                    case "Footsteps/WOOD":
+    //                        footStepAudioSource.PlayOneShot(woodClips[Random.Range(0, woodClips.Length - 1)]);
+    //                        break;
+    //                    case "Footsteps/METAL":
+    //                        footStepAudioSource.PlayOneShot(metalClips[Random.Range(0, metalClips.Length - 1)]);
+    //                        break;
+    //                    case "Footsteps/DIRT":
+    //                        footStepAudioSource.PlayOneShot(dirtClips[Random.Range(0, dirtClips.Length - 1)]);
+    //                        break;
+    //                    case "Footsteps/GRASS": footStepAudioSource.PlayOneShot(grassClips[Random.Range(0, grassClips.Length - 1)]);
+    //                        break;
+    //                    default:
+    //                        break;
+    //                }
+    //            }
+    //
+    //            footstepTimer = GetCurrentOffset;
+    //        }
+    //    } 
 
-        footstepTimer -= Time.deltaTime;
-
-        if (footstepTimer <= 0)
-        {
-            if (Physics.Raycast(playerCamera.transform.position, Vector3.down, out RaycastHit hit, 3))
-            {
-                switch (hit.collider.tag)
-                {
-                    case "Footsteps/WOOD":
-                        footStepAudioSource.PlayOneShot(woodClips[Random.Range(0, woodClips.Length - 1)]);
-                        break;
-                    case "Footsteps/METAL":
-                        footStepAudioSource.PlayOneShot(metalClips[Random.Range(0, metalClips.Length - 1)]);
-                        break;
-                    case "Footsteps/DIRT":
-                        footStepAudioSource.PlayOneShot(dirtClips[Random.Range(0, dirtClips.Length - 1)]);
-                        break;
-                    case "Footsteps/GRASS": footStepAudioSource.PlayOneShot(grassClips[Random.Range(0, grassClips.Length - 1)]);
-                        break;
-                    default:
-                        break;
-                }
-            }
-
-            footstepTimer = GetCurrentOffset;
-        }
-    }
 
     private void UpdateMovementState()
     {
--- Assets/Scripts/Setting/Sound/CharacterStepSounds.cs
+++ Assets/Scripts/Setting/Sound/CharacterStepSounds.cs
@@ -1,7 +1,88 @@
-﻿namespace Setting.Sound
+﻿using UnityEngine;
+using Cinemachine;
+
+namespace Setting.Sound
 {
-    public class CharacterStepSounds
+    public class CharacterStepSounds : MonoBehaviour
     {
+        [SerializeField] private bool useFootsteps = true;
+
+        [Header("Footstep Param")] 
+        [SerializeField] private float baseStepSpeed = 0.5f;
+        [SerializeField] private float crouchStepMultipler = 1.5f;
+        [SerializeField] private float sprintStepMultipler = 0.6f;
+        [SerializeField] private AudioSource footStepAudioSource;
+        [SerializeField] private AudioClip[] woodClips;
+        [SerializeField] private AudioClip[] metalClips;
+        [SerializeField] private AudioClip[] grassClips;
+        [SerializeField] private AudioClip[] dirtClips;
+
+        private CharacterController characterController;
+        private FirstPersonController firstPersonController;
         
+ 
+
+        private float GetCurrentOffset => firstPersonController.isCrouching ? baseStepSpeed * crouchStepMultipler :
+            firstPersonController.IsSprinting ? baseStepSpeed * sprintStepMultipler : baseStepSpeed;
+
+        private Vector3 moveDirection;
+        private Vector2 currentInput;
+        private float footstepTimer = 0;
+
+        private AudioClip GetRandomClip(AudioClip[] clips)
+        {
+            if (clips == null || clips.Length == 0) return null;
+            return clips[Random.Range(0, clips.Length)];
+        }
+
+        public void HandleFootsteps()
+        {
+            if (!characterController.isGrounded) return;
+            if (currentInput == Vector2.zero) return;
+            if (footStepAudioSource == null || !footStepAudioSource.enabled) return;
+
+            footstepTimer -= Time.deltaTime;
+            if (footstepTimer > 0) return;
+
+            if (Physics.Raycast(firstPersonController.playerCamera.transform.position, Vector3.down, out RaycastHit hit, 1.5f))
+            {
+                Debug.Log("Ground Tag: " + hit.collider.tag);
+
+                AudioClip clip = null;
+                switch (hit.collider.tag)
+                {
+                    case "Footsteps/WOOD":
+                        clip = GetRandomClip(woodClips);
+                        break;
+                    case "Footsteps/METAL":
+                        clip = GetRandomClip(metalClips);
+                        break;
+                    case "Footsteps/DIRT":
+                        clip = GetRandomClip(dirtClips);
+                        break;
+                    case "Footsteps/GRASS":
+                        clip = GetRandomClip(grassClips);
+                        break;
+                }
+
+                if (clip != null)
+                    footStepAudioSource.PlayOneShot(clip);
+            }
+
+            footstepTimer = GetCurrentOffset;
+        }
+
+        void Start()
+        {
+            firstPersonController = GetComponent<FirstPersonController>();
+            characterController = GetComponent<CharacterController>();
+        }
+
+        void Update()
+        {
+            currentInput = new Vector2(characterController.velocity.x, characterController.velocity.z);
+            HandleFootsteps();
+        }
     }
+    
 }
