77e826cc0d014bbba3a1589420b01f9be1340215
nekoo7 <abylaiooaooa@mail.ru>
2025-05-23 13:46:43 +0500

    pause menu and scene loader, updgraded bunker

---
+++ Assets/Scripts/ItemSystem/Computer/PauseMenu.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenuUI;
+    public GameObject hub;
+    public string mainMenuSceneName = "MainMenu";
+    public MonoBehaviour playerControllerScript; // сюда перетаскиваешь скрипт игрока
+
+    private bool isPaused = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused){
+                Resume();
+                hub.SetActive(true);
+        }
+        else{
+                Pause();
+                hub.SetActive(false);
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        if (playerControllerScript != null)
+            playerControllerScript.enabled = true;
+
+        isPaused = false;
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+
+        if (playerControllerScript != null)
+            playerControllerScript.enabled = false;
+
+        isPaused = true;
+    }
+
+    public void LoadMainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(mainMenuSceneName);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Выход из игры");
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/ItemSystem/Computer/SceneLoader.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace ItemSystem.Computer
+{
+
+    public class SceneLoader : MonoBehaviour
+    {
+        // Загрузи сцену по её имени
+        public void LoadSceneByName(string sceneName)
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+
+        // Загрузи сцену по индексу
+        public void LoadSceneByIndex(int sceneIndex)
+        {
+            SceneManager.LoadScene(sceneIndex);
+        }
+    }
+}
